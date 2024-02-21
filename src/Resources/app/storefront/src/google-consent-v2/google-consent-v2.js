6725c2a1f094fc6e8fa42e574c403bd10d0effd9
Björn Meyer <b.meyer@shopware.com>
2024-02-21 17:13:16 +0100

    Fix for consent update

--- src/Resources/app/storefront/src/google-consent-v2/google-consent-v2.js
+++ src/Resources/app/storefront/src/google-consent-v2/google-consent-v2.js
@@ -2,24 +2,25 @@ import { COOKIE_CONFIGURATION_UPDATE } from 'src/plugin/cookie/cookie-configurat
 
 document.$emitter.subscribe(COOKIE_CONFIGURATION_UPDATE, eventCallback);
 
-function eventCallback(updatedCookies) {
+function eventCallback(updatedCookiesEvent) {
     this.cookieEnabledName = 'google-analytics-enabled';
     this.cookieAdsEnabledName = 'google-ads-enabled';
 
-    if (Object.keys(updatedCookies).length === 0) {
+    const cookieObject = updatedCookiesEvent.detail;
+    if (Object.keys(cookieObject).length === 0) {
         return;
     }
 
     const consentUpdateConfig = {};
 
-    if (Object.prototype.hasOwnProperty.call(updatedCookies, this.cookieEnabledName)) {
-        consentUpdateConfig['analytics_storage'] = updatedCookies[this.cookieEnabledName] ? 'granted' : 'denied';
+    if (Object.prototype.hasOwnProperty.call(cookieObject, this.cookieEnabledName)) {
+        consentUpdateConfig['analytics_storage'] = cookieObject[this.cookieEnabledName] ? 'granted' : 'denied';
     }
 
-    if (Object.prototype.hasOwnProperty.call(updatedCookies, this.cookieAdsEnabledName)) {
-        consentUpdateConfig['ad_storage'] = updatedCookies[this.cookieAdsEnabledName] ? 'granted' : 'denied';
-        consentUpdateConfig['ad_user_data'] = updatedCookies[this.cookieAdsEnabledName] ? 'granted' : 'denied';
-        consentUpdateConfig['ad_personalization'] = updatedCookies[this.cookieAdsEnabledName] ? 'granted' : 'denied';
+    if (Object.prototype.hasOwnProperty.call(cookieObject, this.cookieAdsEnabledName)) {
+        consentUpdateConfig['ad_storage'] = cookieObject[this.cookieAdsEnabledName] ? 'granted' : 'denied';
+        consentUpdateConfig['ad_user_data'] = cookieObject[this.cookieAdsEnabledName] ? 'granted' : 'denied';
+        consentUpdateConfig['ad_personalization'] = cookieObject[this.cookieAdsEnabledName] ? 'granted' : 'denied';
     }
 
     if (Object.keys(consentUpdateConfig).length === 0) {
